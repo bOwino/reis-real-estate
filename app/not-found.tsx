101c2aa592766100a377b78929865a3c64233dc1
bOwino <brianowino01@gmail.com>
2023-08-17 20:10:52 +0300

    added top navbar

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className='h-fit text-center mt-48'>
+      <h3 className='text-2xl'> 404 | Page Not Found</h3>
+    </div>
+  )
+}
+
+export default NotFound
