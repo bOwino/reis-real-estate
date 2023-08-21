028b14503fbef5029958d8e7b9e2dc4e6679db7b
bOwino <brianowino01@gmail.com>
2023-08-21 22:45:42 +0300

    added sidebar nav on small screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+"use client"
 import "./globals.css"
+import { useState } from "react"
 import type { Metadata } from "next"
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import Link from "next/link"
 import logo from "../public/images/logo.png"
 import { HiMenu } from "react-icons/hi"
+import { GrClose } from "react-icons/gr"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +21,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [sidebarVisible, setSidebarVisible] = useState<boolean>(true)
+
+  function handleSidebarVisibility() {
+    if (sidebarVisible) {
+      setSidebarVisible(false)
+      console.log("set " + sidebarVisible)
+    } else {
+      setSidebarVisible(true)
+      console.log("set " + sidebarVisible)
+    }
+  }
   return (
     <html lang='en'>
       <body className={inter.className}>
@@ -33,8 +47,48 @@ export default function RootLayout({
           </Link>
           <nav>
             <button className='md:hidden cursor-pointer'>
-              <HiMenu className='w-6 h-6' />
+              <HiMenu className='w-5 h-5' onClick={handleSidebarVisibility} />
             </button>
+            <div
+              className={`${
+                sidebarVisible
+                  ? "sidebar md:hidden fixed inset-0 bg-white flex flex-col p-3"
+                  : "hidden"
+              }`}
+            >
+              <button
+                className='flex flex-row justify-end items-end'
+                onClick={handleSidebarVisibility}
+              >
+                <GrClose className='w-6 h-6' />
+              </button>
+              <ul className='md:hidden flex flex-col gap-8 justify-start text-base font-semibold text-gray-700'>
+                <li className='active'>
+                  <Link href='/'>HOME</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='/about'>ABOUT US</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='#'>OUR AGENTS</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='#'>PROPERTIES</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='#'>GALLERY</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='#'>BLOG</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='#'>CONTACT US</Link>
+                </li>
+                <li className='nav-link'>
+                  <Link href='#'>SEARCH</Link>
+                </li>
+              </ul>
+            </div>
             <ul className='hidden md:flex flex-row gap-8 justify-end text-base font-semibold text-gray-700'>
               <li className='active'>
                 <Link href='/'>HOME</Link>
