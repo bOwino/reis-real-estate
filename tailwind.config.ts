b19ffc758781d4a7fc71ee3cd44505d051cba26a
Brian Owino <30558898+bOwino@users.noreply.github.com>
2023-08-23 22:39:38 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,13 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   output: 'export',
+  serverRuntimeConfig: {
+    router: () => {
+      return new NextRouter({
+        basePath: "/",
+      });
+    },
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
